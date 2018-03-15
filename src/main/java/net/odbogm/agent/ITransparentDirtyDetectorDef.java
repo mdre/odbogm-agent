d26ad14b101788aea2e4f38c5f55c0a33b3d7291
Marcelo D. Ré <marcelo.re@gmail.com>
2018-03-15 12:46:49 -0300

    Primer commit con todos los archivos. Se desprende del proyecto ODBOGM.

---
+++ src/main/java/net/odbogm/agent/ITransparentDirtyDetectorDef.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.odbogm.agent;
+
+/**
+ *
+ * @author Marcelo D. Ré {@literal <marcelo.re@gmail.com>}
+ */
+public interface ITransparentDirtyDetectorDef {
+    public final String DIRTYMARK = "___ogm___dirtyMark";
+    public final String ISDIRTY = "___ogm___isDirty";
+    public final String SETDIRTY = "___ogm___setDirty";
+    
+}
---
+++ src/main/java/net/odbogm/agent/LogginProperties.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.odbogm.agent;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Configuración de los log de cada clase
+ * @author Marcelo D. Ré {@literal <marcelo.re@gmail.com>}
+ */
+public class LogginProperties {
+
+    public static Level TransparentDirtyDetector                   = Level.INFO;
+    public static Level TransparentDirtyDetectorAdapter            = Level.FINER;
+    public static Level TransparentDirtyDetectorInstrumentator     = Level.INFO;
+    public static Level TransparentDirtyDetectorAgent              = Level.INFO;
+    public static Level InstrumentableClassDetector                = Level.INFO;
+    public static Level WriteAccessActivatorAdapter                = Level.INFO;
+    
+    
+}
